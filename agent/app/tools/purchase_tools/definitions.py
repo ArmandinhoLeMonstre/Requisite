eebaea949d1d193703c517cdfb1d3314993310bf
rafnasci <rafael.nascimento@outlook.be>
2026-04-01 19:38:44 +0200

    main workflow of the purchase agent done with the tools added

--- agent/app/tools/purchase_tools/definitions.py
+++ agent/app/tools/purchase_tools/definitions.py
@@ -1,1 +1,17 @@
-TOOLS = []
+tools = [
+    {
+        "type": "function",
+        "name": "get_product",
+        "description": "Get the price, link and thumbnail of an amazon product using its ASIN thanks to serpAPI",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "asin": {
+                    "type": "string",
+                    "description": "an ASIN e.g. B01N2KFHRS",
+                },
+            },
+            "required": ["asin"],
+        },
+    },
+]
