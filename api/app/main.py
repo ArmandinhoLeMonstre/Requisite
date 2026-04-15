4195f6dab912a69f5f8c545f5a521b7248a566bf
rafnasci <rafael.nascimento@outlook.be>
2026-04-15 20:44:33 +0200

    reset du fichier, suppresion de fichier inutile, modification du docker-compose et du main et update des requirements.txt

--- api/app/main.py
+++ api/app/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from app.routers.shop_router import router as shop_router
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # db_conn_test()
 
-    yield
+app = FastAPI()
 
-app = FastAPI(lifespan=lifespan)
-
-app.include_router(shop_router)
+@app.get("/")
+def root():
+    return {"Petit", "Zeub"}
