226af49750e48fd511fbbef56b35af71afc96b02
rafnasci <rafael.nascimento@outlook.be>
2026-04-22 11:38:20 +0200

    add a check to see if the user.name and user.email already exists when creating a new user, and updating little things in tickets and user services

--- api/app/services/ticket_services.py
+++ api/app/services/ticket_services.py
@@ -12,17 +12,20 @@ def create_ticket(ticket: RequestTicket, db: Session):
 		description= ticket.description,
 		user_id= ticket.user_id,
 	)
+
 	try:
 		user = db.scalars(select(User).where(User.id == ticket.user_id)).one()
 		user.tickets.append(ticket_stmt)
 	except SQLAlchemyError:
 		raise HTTPException(status_code=404, detail="User not found")
+	
 	try:
 		db.add(ticket_stmt)
 		db.commit()
+		db.refresh(ticket_stmt)
 	except SQLAlchemyError:
 		raise HTTPException(status_code=500, detail="Error with Database server")
-	db.refresh(ticket_stmt)
+	
 	return ticket_stmt
 
 
@@ -31,4 +34,5 @@ def select_ticket(ticket_id: uuid.UUID, db: Session):
 		ticket = db.scalars(select(Ticket).where(Ticket.id == ticket_id)).one()
 	except SQLAlchemyError:
 		raise HTTPException(status_code=404, detail="Ticket not found")
+	
 	return ticket
--- api/app/services/user_services.py
+++ api/app/services/user_services.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy.exc import SQLAlchemyError, NoResultFound, DatabaseError
 from app.models.user import User
 from app.init_db import engine, Session, select
@@ -7,26 +6,34 @@ from fastapi import HTTPException
 
 
 def create_user(user: RequestUser, db: Session):
+	existing = db.scalars(select(User).where((User.name == user.name) | (User.email == user.email))).first()
+	if existing:
+		if existing.name == user.name:
+			raise HTTPException(status_code=400, detail="Name already exists")
+		raise HTTPException(status_code=400, detail="Email already exists")
+	
 	user_stmt = User(
 		name= user.name,
 		email= user.email,
 		role= user.role,
 		hashed_password= user.hashed_password
 	)
+
 	try:
 		db.add(user_stmt)
 		db.commit()
+		db.refresh(user_stmt)
 	except SQLAlchemyError:
 		raise HTTPException(status_code=500, detail="Error with Database server")
-	db.refresh(user_stmt)
+
 	return user_stmt
 
 def select_user(id: int, db: Session):
 	try:
-		stmt = select(User).where(User.id == id)
-		user = db.scalars(stmt).one()
+		user = db.scalars(select(User).where(User.id == id)).one()
 	except NoResultFound:
 		raise HTTPException(status_code=404, detail="User not found")
 	except SQLAlchemyError:
 		raise HTTPException(status_code=500, detail="Error with Database server")
+
 	return(user)
