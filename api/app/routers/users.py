1d79b422794316ab052ada8b1ec6338070c5f007
rafnasci <rafael.nascimento@outlook.be>
2026-04-25 19:11:31 +0200

    add a function that created a token with the endpoint /token, added the route

--- api/app/routers/users.py
+++ api/app/routers/users.py
@@ -5,9 +5,14 @@ from typing import Annotated
 from app.init_db import get_db 
 from sqlalchemy.orm import Session
 
-from app.schemas.user_schemas import UserCreate, UserPublic, UserUpdate, UserPrivate
+from app.schemas.user_schemas import UserCreate, UserPublic, UserUpdate, UserPrivate, Token
 from app.services.user_services import create_user, select_user, patch_user, delete_user
+from app.services.token_services import log_for_access_token
 
+from datetime import timedelta
+from fastapi.security import OAuth2PasswordRequestForm
+from app.auth import hash_password, oauth2_scheme, verify_password, create_access_token, verify_access_token
+from app.config import settings
 
 router = APIRouter()
 
@@ -15,6 +20,13 @@ router = APIRouter()
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=UserPrivate)
 def post_new_user(user: UserCreate, db:Annotated[Session, Depends(get_db)]):
     return create_user(user, db)
+
+@router.post("/token", response_model=Token)
+def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Annotated[Session, Depends(get_db)],
+):
+    return log_for_access_token(form_data, db)
 
 @router.get("/{user_id}", response_model=UserPublic)
 def get_user(user_id: int, db:Annotated[Session, Depends(get_db)]):
