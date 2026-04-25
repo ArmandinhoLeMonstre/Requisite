285dfdbef076b22a1133d2959bc37e7900719005
rafnasci <rafael.nascimento@outlook.be>
2026-04-25 19:41:33 +0200

    changed in the user_scheme for the group_id

--- api/app/schemas/user_schemas.py
+++ api/app/schemas/user_schemas.py
@@ -8,7 +8,6 @@ class UserBase(BaseModel):
 	name: str = Field(min_length=1, max_length=50)
 	email: EmailStr = Field(max_length=120)
 	role: UserRole
-	group_id: Optional[int] = None
 
 
 class UserCreate(UserBase):
@@ -30,7 +29,7 @@ class UserPublic(BaseModel):
 	id: int
 	name: str
 	role: UserRole
-	group_id: int
+	group_id: Optional[int] = None
 	tickets: list[TicketResponse]
 
 
