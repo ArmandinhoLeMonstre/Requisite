f294128654dfbbab8483ff5e3d235919d8da8fe3
rafnasci <rafael.nascimento@outlook.be>
2026-04-25 19:08:05 +0200

    updated the functions so that the comparison between email and name are done after putting eveything to lower case and update of the create_user function so that we put a hashed password in the DB

--- api/app/services/user_services.py
+++ api/app/services/user_services.py
@@ -1,14 +1,16 @@
-from sqlalchemy.exc import SQLAlchemyError, NoResultFound, DatabaseError
+from sqlalchemy.exc import SQLAlchemyError, NoResultFound
+from sqlalchemy import func
 from app.models.user import User
 from app.models.group import Group
-from app.init_db import engine, Session, select
+from app.init_db import Session, select
 from app.schemas.user_schemas import UserCreate, UserUpdate
 from fastapi import HTTPException
+from app.auth import hash_password
 
 
 def create_user(user: UserCreate, db: Session):
 	try:
-		existing = db.scalars(select(User).where((User.name == user.name) | (User.email == user.email))).first()
+		existing = db.scalars(select(User).where((func.lower(User.name) == user.name.lower()) | (func.lower(User.email) == user.email.lower()))).first()
 	except SQLAlchemyError:
 		raise HTTPException(status_code=500, detail="Error with database")
 	if existing:
@@ -16,21 +18,21 @@ def create_user(user: UserCreate, db: Session):
 			raise HTTPException(status_code=400, detail="Name already exists")
 		raise HTTPException(status_code=400, detail="Email already exists")
 	
-	user_stmt = User(
+	new_user = User(
 		name= user.name,
-		email= user.email,
+		email= user.email.lower(),
 		role= user.role,
-		hashed_password= user.hashed_password
+		hashed_password= hash_password(user.password)
 	)
 
 	try:
-		db.add(user_stmt)
+		db.add(new_user)
 		db.commit()
-		db.refresh(user_stmt)
+		db.refresh(new_user)
 	except SQLAlchemyError:
 		raise HTTPException(status_code=500, detail="Error with Database server")
 
-	return user_stmt
+	return new_user
 
 def select_user(id: int, db: Session):
 	try:
@@ -44,8 +46,8 @@ def select_user(id: int, db: Session):
 
 def patch_user(user_id: int, new_data: UserUpdate, db: Session):
 	REGISTRY = {
-		"name": User.name,
-		"email": User.email,
+		"name": func.lower(User.name),
+		"email": func.lower(User.email),
 	}
 
 	try:
@@ -67,7 +69,7 @@ def patch_user(user_id: int, new_data: UserUpdate, db: Session):
 				raise HTTPException(status_code=500, detail="Error with Database server")
 		elif key == "name" or key == "email":
 			try:
-				existing = db.scalars(select(User).where((REGISTRY.get(key) == value) & (User.id != user_id))).first()
+				existing = db.scalars(select(User).where((REGISTRY.get(key) == value.lower()) & (User.id != user_id))).first()
 			except SQLAlchemyError:
 				raise HTTPException(status_code=500, detail="Error with Database server")
 			if existing:
