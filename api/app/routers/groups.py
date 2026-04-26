2f6ab25c808eabce118af9124a35081aff56fbe9
rafnasci <rafael.nascimento@outlook.be>
2026-04-26 16:45:45 +0200

    added the protections for the users, now using the dependency and new function to join group, still have to add a route for that

--- api/app/routers/groups.py
+++ api/app/routers/groups.py
@@ -7,14 +7,15 @@ from sqlalchemy.orm import Session
 
 from app.schemas.group_schemas import GroupCreate, GroupResponse
 from app.services.group_services import create_group, select_group
+from app.services.user_services import CurrentUser
 
 
 router = APIRouter()
 
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=GroupResponse)
-def post_new_group(group: GroupCreate, db: Annotated[Session, Depends(get_db)]):
-    return create_group(group, db)
+def post_new_group(group: GroupCreate, current_user: CurrentUser, db: Annotated[Session, Depends(get_db)]):
+    return create_group(group, current_user, db)
 
 @router.get("/{group_id}", response_model=GroupResponse)
-def get_group(group_id: int, db: Annotated[Session, Depends(get_db)]):
-    return select_group(group_id, db)
+def get_group(group_id: int, current_user: CurrentUser,  db: Annotated[Session, Depends(get_db)]):
+    return select_group(group_id, current_user, db)
--- api/app/services/group_services.py
+++ api/app/services/group_services.py
@@ -1,28 +1,27 @@
 from sqlalchemy.exc import SQLAlchemyError, NoResultFound
+from sqlalchemy import func, select
+from sqlalchemy.orm import Session
+
 from app.models.group import Group
-from app.init_db import engine, select, Session
 from app.models.user import User, UserRole
 from app.schemas.group_schemas import GroupCreate
-from fastapi import HTTPException
+from fastapi import HTTPException, status
+
 import random
 import string
 
 
-def create_group(group: GroupCreate, db: Session):
-	try:
-		manager = db.scalars(select(User).where(User.id == group.manager_id)).one()
-	except SQLAlchemyError:
-		raise HTTPException(status_code=404, detail="User not found")
-	if manager.role != UserRole.manager:
-		raise HTTPException(status_code=403, detail="User is not a manager")
+def create_group(group: GroupCreate, current_user: User, db: Session):
+	if current_user.role != UserRole.manager:
+		raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is not a manager")
 	
 	group_stmt = Group(
 		code= ''.join(random.choices(string.ascii_uppercase + string.digits, k=6)),
-		manager_id= group.manager_id
+		manager_id= current_user.id
 	)
 
 	try:
-		manager.group = group_stmt
+		current_user.group = group_stmt
 		db.add(group_stmt)
 		db.commit()
 		db.refresh(group_stmt)
@@ -32,7 +31,10 @@ def create_group(group: GroupCreate, db: Session):
 	return (group_stmt)
 
 
-def select_group(group_id: int, db: Session):
+def select_group(group_id: int, current_user: User, db: Session):
+	if current_user.role != UserRole.manager:
+		raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is not manager")
+
 	try:
 		group = db.scalars(select(Group).where(Group.id == group_id)).one()
 	except NoResultFound:
@@ -40,4 +42,17 @@ def select_group(group_id: int, db: Session):
 	except SQLAlchemyError:
 		raise HTTPException(status_code=500, detail="Error with Database server")
 	
+	return group
+
+
+def join_group(current_user: User, code: str, db: Session):
+	try:
+		group = db.scalars(select(Group).where(func.lower(Group.code) == code.lower())).one()
+	except NoResultFound:
+		raise HTTPException(status_code= 404, detail="Code doesn't belong to a group")
+	except SQLAlchemyError:
+		raise HTTPException(status_code=500, detail="Error with Database server")
+	
+	current_user.group = group
+
 	return group
