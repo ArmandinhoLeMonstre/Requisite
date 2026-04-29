04686dc0a01f6bf1c62c9339257b4f705cd2fa2f
rafnasci <rafael.nascimento@outlook.be>
2026-04-29 20:48:25 +0200

    refactor du fichier invetory agent

--- api/app/agents_app/tools/request_tools/registry.py
+++ api/app/agents_app/tools/request_tools/registry.py
@@ -1,4 +1,4 @@
-from app.agents_app.agents.agent_inventory.agent_inventory_check import call_inventory_agent
+from app.agents_app.agents.inventory_agent import call_inventory_agent
 from app.agents_app.agents.agent_purchase.amz_search_agent import call_amazon_agent
 from app.agents_app.agents.agent_purchase.email_agent import call_email_agent
 
