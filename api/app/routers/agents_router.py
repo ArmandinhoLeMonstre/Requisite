9c7ceb615c86207f832c70d5c70576c015389e4d
rafnasci <rafael.nascimento@outlook.be>
2026-05-01 08:45:13 +0200

    orchestratorRequest to OrchestratorData, nothing much

--- api/app/routers/agents_router.py
+++ api/app/routers/agents_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.schemas.agents_requests_schemas import OrchestratorRequest, OrchestratorResponse
+from app.schemas.agents_requests_schemas import OrchestratorData, OrchestratorResponse
 import app.services.orchestrator_service as orchestrator_service
 import app.services.db_service as db_service
 from typing import Annotated
@@ -9,7 +9,7 @@ from app.database import get_db
 router = APIRouter()
 
 @router.post("", response_model=OrchestratorResponse)
-def call_agents_orchestrator(req: OrchestratorRequest, db: Annotated[Session, Depends(get_db)]):
+def call_agents_orchestrator(req: OrchestratorData, db: Annotated[Session, Depends(get_db)]):
 	existing_input_list = db_service.retrieve_input_list(db, req.ticket_id)
 
 	result = orchestrator_service.send_request_to_orchestrator(req, existing_input_list)
