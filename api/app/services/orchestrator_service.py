f72d665b6dd83c0328e528cda9c4ef6f0f5f8d00
rafnasci <rafael.nascimento@outlook.be>
2026-05-01 08:48:24 +0200

    added 2 servies for the orchestrator, create_data to get all the needed data from the db to pass it to the orchestrator and the call_agent_orchestrator that was route previously and was transformed into a service, changed it a little bit to not have hard coded data

--- api/app/services/orchestrator_service.py
+++ api/app/services/orchestrator_service.py
@@ -1,8 +1,15 @@
-from app.schemas.agents_requests_schemas import OrchestratorRequest
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound, SQLAlchemyError
+from app.models.user_model import User
+from app.models.ticket_model import Ticket
+from app.schemas.agents_requests_schemas import OrchestratorResponse
+from app.schemas.agents_requests_schemas import OrchestratorData
 from app.agents_app.agents.orchestrator_agent import call_orchestrator_agent
 from app.agents_app.agents_exceptions import OrchestratorError
 from fastapi import HTTPException, status
 from openai import OpenAI
+import app.services.db_service as db_service
 import app.services.formatter_service as formatter_service
 import os
 import json
@@ -12,29 +19,13 @@ client = OpenAI(
 	api_key=openai_api_key
 )
 
-def send_request_to_orchestrator(req: OrchestratorRequest, existing_input_list: list):
+def send_request_to_orchestrator(message: str, existing_input_list: list, data: OrchestratorData):
 
-	data = {
-		"employee": {
-			"name": "Ricardo",
-			"department": "Marketing",
-			"email": "rafael.nascimento@outlook.be"
-		},
-		"manager": {
-			"name": "Armand",
-			"email": "armandeuarmand@gmail.com"
-		},
-		"ticket": {
-			"id": "#6242",
-			"reason": "Need approvisionnement",
-			"created_at": "08/04/2026"
-		}
-	}
 	req_input_list = []
 	if existing_input_list:
 		req_input_list.extend(existing_input_list)
 
-	req_input_list.extend([{"role": "user", "content": req.user_message}])
+	req_input_list.extend([{"role": "user", "content": message}])
 
 	try:
 		orchestrator_response = call_orchestrator_agent(client, data, req_input_list)
@@ -50,3 +41,39 @@ def send_request_to_orchestrator(req: OrchestratorRequest, existing_input_list: 
 		"history": updated_input_list,
 		"message": orchestrator_response.get("message")
 	})
+
+
+def create_data(current_user: User, db : Session, ticket: Ticket, msg: str):
+	try:
+		manager = db.scalars(select(User).where(User.id == current_user.group.manager_id)).one()
+	except NoResultFound:
+		raise HTTPException(status_code=404, detail="Manager not found")
+	except SQLAlchemyError:
+		raise HTTPException(status_code=500, detail="Error with Database server")
+
+	data = OrchestratorData(
+		manager_name= manager.name,
+		manager_email= manager.email,
+		user_name= current_user.name,
+		user_email= current_user.email,
+		ticket_id= ticket.id,
+		created_at= ticket.created_at,
+		user_message= msg
+	)
+
+	return data
+
+
+def call_agents_orchestrator(data: OrchestratorData, db: Session, message: str):
+	existing_input_list = db_service.retrieve_input_list(db, data.ticket_id)
+
+	result = send_request_to_orchestrator(message, existing_input_list, data)
+
+	db_service.save_input_list(db, data.ticket_id, result.get("history"))
+
+	response = OrchestratorResponse(
+		ticket_id=data.ticket_id,
+		orchestrator_message=result.get("message")
+	)
+
+	return (response)
