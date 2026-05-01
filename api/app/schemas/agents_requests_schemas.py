2dd0e2fe13ba27d4359e026a205144ff885edb24
rafnasci <rafael.nascimento@outlook.be>
2026-05-01 08:44:32 +0200

    changed the OrchestratorRequest to OrchestratorData so it can hold all the data needed to for the oechestrator

--- api/app/schemas/agents_requests_schemas.py
+++ api/app/schemas/agents_requests_schemas.py
@@ -1,9 +1,17 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
+from datetime import datetime
+from app.schemas.chat_schemas import ChatRequest
+import uuid
 
-class OrchestratorRequest(BaseModel):
-	ticket_id: int = Field(gt=0)
+class OrchestratorData(BaseModel):
+	manager_name : str = Field(description="Manager's name", min_length=1)
+	manager_email: EmailStr = Field(description="Manager's email")
+	user_name: str = Field(description="User's name", min_length=1)
+	user_email: EmailStr = Field(description="User's email")
+	ticket_id: uuid.UUID = Field(description="Ticket's id")
+	created_at: datetime = Field(description="Date of ticket creation")
 	user_message: str = Field(description="User's message to the orchestrator", min_length=1)
 
 class OrchestratorResponse(BaseModel):
-	ticket_id: int = Field(gt=0)
+	ticket_id: uuid.UUID = Field(description="Ticket's id")
 	orchestrator_message: str = Field(description="Orchestrator's message to the user", min_length=1)
