8de90225ab9bf2a59eeacb0167d3b46aa65e976b
rafnasci <rafael.nascimento@outlook.be>
2026-05-13 16:33:37 +0200

    only localhost now

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:8080/api",
-  baseURL: "http://192.168.0.84:8080/api",
+  baseURL: "http://localhost:8080/api",
 });
 
 function getToken() {
