d69c81ac5208f4317f1556ab9c0423199696cefd
rafnasci <rafael.nascimento@outlook.be>
2026-05-22 19:03:21 +0200

    added a limit to 20 chats per ticket, when the limit is reached the ticket status becomes pending

--- api/app/services/chat_services.py
+++ api/app/services/chat_services.py
@@ -1,11 +1,11 @@
-from sqlalchemy import select
+from sqlalchemy import select, func, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError, NoResultFound
 
 from fastapi import HTTPException, status
 
 from app.models.chat_model import Chat, Sender
-from app.models.ticket_model import Ticket
+from app.models.ticket_model import Ticket, TicketStatus
 from app.models.user_model import User
 from app.schemas.chat_schemas import ChatResponse, ChatRequest
 from app.schemas.ticket_schemas import TicketResponse
@@ -38,3 +38,31 @@ async def new_message(chat: ChatRequest, db: AsyncSession, ticket: TicketRespons
 	logger.info("chat.added", ticket_id=ticket.id)
 
 	return chat
+
+async def count_chats(ticket: TicketResponse, db: AsyncSession):
+	try:
+		stmt = await db.execute(
+			select(func.count())
+			.select_from(Chat)
+			.where(Chat.ticket_id == ticket.id)
+		)
+		chats = stmt.scalar()
+	except Exception as e:
+		logger.error("object.create.error", error=str(e), step="check_current_manager_objects")
+		raise(HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error with database"))
+
+	if chats >= 20 :
+		print("OUIIIII")
+		try:
+			stmt = await db.execute(
+				update(Ticket)
+				.where(Ticket.id == ticket.id)
+				.values(status=TicketStatus.pending)
+				.returning(Ticket.id)
+			)
+			await db.commit()
+		except SQLAlchemyError as e:
+			logger.error("Update Ticket error", error=str(e), step="Retrieve Ticket in count_chat")
+			raise HTTPException(status_code=500, detail="Error with Database server")
+
+	return
